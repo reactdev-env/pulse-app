7b4e399c9b3045349e4d539b2ef319f367f6d4a8
pavan sai <pavansaipcr@gmail.com>
2025-08-27 00:48:41 +0530

    The Final router,params,searchParams,pathname

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,15 +1,17 @@
 'use client'
-import { useRouter,useParams, useSearchParams } from "next/navigation";
+import { useRouter,useParams, useSearchParams,usePathname } from "next/navigation";
 
 const PropertyPage = () => {
   const router = useRouter();
   const params = useParams();
   const searchParams = useSearchParams();
+  const pathname = usePathname();
   console.log(router)
   return (
     <div>
     <h1>Property Page {params.id}</h1>
     <h1>Property Page {searchParams.get('name')}</h1>
+    <h1>Property Page {pathname}</h1>
 
       <button onClick={() => router.replace('/')}>Go home</button>
     </div>
