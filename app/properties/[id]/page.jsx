970d7d32dc19fadc8c82f56aa70eb675a3675d8b
pavan sai <pavansaipcr@gmail.com>
2025-08-27 00:42:51 +0530

    use params

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,12 +1,13 @@
 'use client'
-import { useRouter } from "next/navigation";
+import { useRouter,useParams } from "next/navigation";
 
 const PropertyPage = () => {
   const router = useRouter();
+  const params = useParams();
   console.log(router)
   return (
     <div>
-    <h1>He I am the nth number of properties</h1>
+    <h1>Property Page {params.id}</h1>
       <button onClick={() => router.replace('/')}>Go home</button>
     </div>
   )
